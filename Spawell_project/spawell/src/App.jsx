4e659b2255814616428c254dadfb6a4d3c6edf4e
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-04-15 11:28:37 +0530

    file update

---
+++ Spawell_project/spawell/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='heading'>Hello world!</h1>
+    <h2>Tailwinds</h2>
+    <h5>આ લીલા રંગમાં દેખાશે (Base layer)</h5>
+    </>
+  )
+}
+
+export default App
