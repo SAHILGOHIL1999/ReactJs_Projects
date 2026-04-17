97115d3c5ce4efcb7389ee75aab3ea84aed5f55b
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-04-17 10:00:47 +0530

    file update

---
+++ Spawell_project/spawell/src/pages/All_pages.jsx
@@ -0,0 +1,18 @@
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Blog from "./Blog";
+import Pages from "./Pages";
+import Contact from "./Contact";
+
+const All_pages = {
+    Home,
+    About_Us,
+    Services,
+    Blog,
+    Pages,
+    Contact_US,
+    Error
+}
+
+export default All_pages
---
+++ Spawell_project/spawell/src/pages/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createBrowserRouter, createBrowserRouter as Router, RouterProvider } from "react-router-dom";
+import All_pages from "./All_pages";
+
+
+const router = createBrowserRouter([
+    {
+
+    }
+])
